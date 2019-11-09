34a75c50c85a156693de332da54442b30c4afb87
mytaihong <794166177@qq.com>
2019-11-09 17:33:44 +0800

    测试3

--- view/src/main/java/com/example/view/view/ScanBarView.java
+++ view/src/main/java/com/example/view/view/ScanBarView.java
@@ -48,6 +48,7 @@ public class ScanBarView extends SurfaceView implements SurfaceHolder.Callback {
     public ScanBarView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
         init();
+        init();
     }
 
     private void init() {
